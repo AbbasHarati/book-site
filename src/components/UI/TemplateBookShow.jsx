0e2f3175ceace8848b6f43a584da93fe81b35653
AbbasHarati <harati.ab@gmail.com>
2022-08-28 22:08:20 +0430

    first initial sort

--- src/components/UI/TemplateBookShow.jsx
+++ src/components/UI/TemplateBookShow.jsx
@@ -1,7 +1,7 @@
 ////   ایجاد صفحه با ساختار ثابت برای نمایش کتاب های تخصص های مختلف در آن
 
 
-import React from 'react'
+import React, { useState } from 'react'
 import { Container, Row, Col, Button } from 'react-bootstrap'
 import booksData from '../../assets/data/booksData'
 import BookItem from './BookItem'
@@ -9,51 +9,54 @@ import '../../styles/template-book-show.css'
 
 
 const TemplateBookShow = (props) => {
+    const [booksDataObject, setBookDataObject] = useState(booksData);
 
-    
-        const bookAscendingSort = [...booksData].sort((a, b) =>
-            a.title > b.title ? 1 : -1,
-        );   
-         console.log(bookAscendingSort);
-  
+    const sortByName = () => {
+        setBookDataObject((booksData) => {
+            const dataTosort = [...booksData].sort((a, b) =>
+                a.title > b.title ? 1 : -1,
+            );
+            return dataTosort;
+        });
+    }
 
 
-    return (
-        <Container>
-            <Row>
-                <Col lg='4' className="filter__section">
-                    <div>
-                        Filters
-                    </div>
-                </Col>
+return (
+    <Container>
+        <Row>
+            <Col lg='4' className="filter__section">
+                <div>
+                    Filters
+                </div>
+            </Col>
 
-                <Col lg='8' className="result__section">
-                    <div >
-                        <Row>
-                            <div className="title__section">
-                                <h3>{props.title}</h3>
-                            </div>
-                        </Row>
-                        <Row>
-                            <div className="sort__section">
-                                <Button >
-                                    sort by name
-                                </Button>
-                            </div>
-                        </Row>
-                        <Row className="show__section">
-                            {bookAscendingSort.filter(book => (props.category == book.category || props.category == book.other_category))
-                                .map((book) => (
-                                    <BookItem item={book} key={book.id} />
-                                ))
-                            }
-                        </Row>
-                    </div>
-                </Col>
-            </Row>
-        </Container>
+            <Col lg='8' className="result__section">
+                <div >
+                    <Row>
+                        <div className="title__section">
+                            <h3>{props.title}</h3>
+                        </div>
+                    </Row>
+                    <Row>
+                        <div className="sort__section">
+                            <Button onClick={sortByName}>
+                                sort by name
+                            </Button>
+                        </div>
+                    </Row>
+                    <Row className="show__section">
+                        {booksDataObject.filter(book => (props.category == book.category || props.category == book.other_category))
+                            .map((book) => (
+                                <BookItem item={book} key={book.id} />
+                            ))
+                        }
+                    </Row>
+                </div>
+            </Col>
+        </Row>
+    </Container>
 
-    )
+)
 }
 
 export default TemplateBookShow
