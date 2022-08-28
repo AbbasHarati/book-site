c52780ea4b3332e5397ea03b2b04db53ccf53b02
AbbasHarati <harati.ab@gmail.com>
2022-08-28 21:29:32 +0430

    add first sort method

--- src/components/UI/BookItem.jsx
+++ src/components/UI/BookItem.jsx
@@ -1,27 +1,30 @@
 import React from 'react'
-import {Col} from 'react-bootstrap'
+import { Col } from 'react-bootstrap'
+import '../../styles/book-item.css'
 
 
 const BookItem = (props) => {
-    const { imgUrl, title, author, category } = props.item;
-    return (
-          <Col lg="3" md="3" sm="6" className="mb-5 mt-5">
-            <div className="book__info">
-              <div className="book__img">
-                <img src={imgUrl} width='220' height='300' alt="" className="img__responsive w-100" typeof="foaf:Image" />
-              </div>
-              <div className="book__title">
-                <h4>{title}</h4>
-              </div>
-              <div className="book__author">
-                <h5>{author}</h5>
-              </div>
-              <div className="book__category">
-                <h6>{category}</h6>
-              </div>
-            </div>
-          </Col>
-    )
-  }
+  const { imgUrl, title, edition, author } = props.item;
+  return (
+    <Col lg="3" md="4" sm="6" className='col__section'>
+      <div className="book__info m-2" >
+        <div className="book__img">
+          <img src={imgUrl} width='170' height='260' alt="" className="img__responsive w-80" typeof="foaf:Image" />
+        </div>
+        <div className="book__details">
+          <div className="book__title">
+            <p><strong>{title}</strong></p>
+          </div>
+          <div className="book__edition">
+            <p>{edition}</p>
+          </div>
+          <div className="book__author">
+            <p>{author}</p>
+          </div>
+        </div>
+      </div>
+    </Col>
+  )
+}
 
-  export default BookItem;
+export default BookItem;
--- src/components/UI/TemplateBookShow.jsx
+++ src/components/UI/TemplateBookShow.jsx
@@ -2,7 +2,7 @@
 
 
 import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col, Button } from 'react-bootstrap'
 import booksData from '../../assets/data/booksData'
 import BookItem from './BookItem'
 import '../../styles/template-book-show.css'
@@ -10,23 +10,43 @@ import '../../styles/template-book-show.css'
 
 const TemplateBookShow = (props) => {
 
+    
+        const bookAscendingSort = [...booksData].sort((a, b) =>
+            a.title > b.title ? 1 : -1,
+        );   
+         console.log(bookAscendingSort);
+  
+
+
     return (
         <Container>
             <Row>
-                <Col lg='4'>
-                    <div className="filter__section">
+                <Col lg='4' className="filter__section">
+                    <div>
                         Filters
                     </div>
                 </Col>
 
-                <Col lg='8'>
-                    <div className="result__section">
-                        <Row><h3>{props.title}</h3></Row>
-                        <Row>sort</Row>
+                <Col lg='8' className="result__section">
+                    <div >
                         <Row>
-                            {booksData.filter(book => (props.category==book.category || props.category==book.other_category)).map((item) => (
-                                <BookItem item={item} key={item.id} />
-                            ))}
+                            <div className="title__section">
+                                <h3>{props.title}</h3>
+                            </div>
+                        </Row>
+                        <Row>
+                            <div className="sort__section">
+                                <Button >
+                                    sort by name
+                                </Button>
+                            </div>
+                        </Row>
+                        <Row className="show__section">
+                            {bookAscendingSort.filter(book => (props.category == book.category || props.category == book.other_category))
+                                .map((book) => (
+                                    <BookItem item={book} key={book.id} />
+                                ))
+                            }
                         </Row>
                     </div>
                 </Col>
